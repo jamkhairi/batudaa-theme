6fe972b25b7f29f496f0b1108c04622dd5f20318
Abdul Gani <buabatudaa@gmail.com>
2019-03-12 00:42:42 +0800

    Penyesuaian fasilitas lapor di layanan mandiri opensid versi 19.x

--- batudaa/partials/mandiri/lapor.php
+++ batudaa/partials/mandiri/lapor.php
@@ -2,16 +2,16 @@
 
 <div class="list-frame">
 	<h2 class='text-title mt0'>LAPORAN PENDUDUK</h2>
-	<?php
-	if ($_SESSION['sukses']==1){
-		echo "<div class='alert alert-success'>Data telah terkirim, dan akan segera kami proses.</div>";
-		unset($_SESSION['sukses']);
-	}
-	?>
+
+	<!-- Tampilkan hanya jika 'flash_message' ada -->
+	<?php $label = !empty($_SESSION['validation_error']) ? 'alert-danger' : 'alert-success'; ?>
+	<?php if ($flash_message): ?>
+		<div class="alert <?= $label?>"><?= $flash_message?></div>
+	<?php endif; ?>
 
 	<p class='text-warning'><i class='fa fa-comments-o'></i> Silahkan laporkan perubahan data kependudukan anda.</p>
 
-	<form id="validasi" action="<?php echo site_url('first/add_comment/775') ?>" method="POST">
+	<form id="validasi" action="<?php echo site_url('lapor/insert') ?>" method="POST" onSubmit="return validasi(this);">
 	<div class='form-group'>
 		<label>Pengirim</label>
 		<input class="form-control" type="text" readonly="readonly" name="owner" value="<?php echo $_SESSION['nama']?>"/>
